3a0ff3b3158df314b0257719ec651eafa0af8597
oliverloki <oliverloki@foxmail.com>
2022-04-13 00:44:31 +0800

    虚拟机类加载器更新

---
+++ JavaSE/JVM/jvmCode/src/main/java/StackStructureTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author oliverloki
+ * @Description:
+ * @date 2022年04月12日 16:22
+ */
+public class StackStructureTest {
+
+    public static void main(String[] args) {
+        int a = 1 + 12;
+    }
+}
